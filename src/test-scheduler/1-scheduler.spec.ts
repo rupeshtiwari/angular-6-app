ccf9a160546da6f4217fd8803d6cebb574809dcc
roopkt@gmail.com <roopkt@gmail.com>
2018-10-10 21:17:31 -0400

    adding more test

--- src/test-scheduler/1-scheduler.spec.ts
+++ src/test-scheduler/1-scheduler.spec.ts
@@ -1,4 +1,10 @@
-import { hot, getTestScheduler, cold, initTestScheduler } from 'jasmine-marbles';
+import {
+  hot,
+  getTestScheduler,
+  cold,
+  initTestScheduler,
+  resetTestScheduler
+} from 'jasmine-marbles';
 import { interval } from 'rxjs';
 import { take, filter } from 'rxjs/operators';
 
@@ -15,31 +21,47 @@ describe('testscheduler', () => {
      */
 
     const expected = cold('--a-b-c-(d|)', { a: 0, b: 1, c: 2, d: 3, e: 4 });
-
+    // show them that it can go max 740 frames + 0 frame = 750 frames.
+    // expect(source).toBeObservable(cold('--a-b-c-d-e-f-'));
     expect(result).toBeObservable(expected);
   });
 
   it('should work with interval operators with large time', () => {
-    initTestScheduler();
     const scheduler = getTestScheduler();
-
 
     const source = interval(80, scheduler);
     const result = source.pipe(take(4));
 
     /**
-     * 80ms 0 159ms 1 239ms 2 319ms 3 79ms 4 79ms 5
+     * ---------0-------1-------2-------3-------4-------5
      *   take(4)
-     * 80ms 0 159ms 1 239ms 2 319ms (3|)
+     * ---------0-------1-------2-------(3|)
      */
 
-    const expected = cold('--------a 159ms b 239ms c 319ms (d|)', {
+    const expected = cold('--------a-------b-------c-------(d|)', {
       a: 0,
       b: 1,
       c: 2,
       d: 3,
       e: 4
     });
+
+    expect(result).toBeObservable(expected);
+  });
+
+  it('should work with interval and filter operators', () => {
+    const data = { a: 0, b: 1, c: 2, d: 3, e: 4 };
+    const scheduler = getTestScheduler();
+    const source = interval(20, scheduler).pipe(take(4));
+    const result = source.pipe(filter(x => x % 2 === 0));
+
+    /**
+     * --0-1-2-(3|)
+     *   filter(EVEN NUMBERS)
+     * --0---2-(-|)
+     */
+
+    const expected = cold('--a---c-(-|)', data);
 
     expect(result).toBeObservable(expected);
   });
