4627c0e246267a0fd29d9ae082f1c4f3e8db8151
roopkt@gmail.com <roopkt@gmail.com>
2018-10-27 08:08:05 -0400

    organizing test

--- src/marble-syntax/marble-syntax.spec.ts
+++ src/marble-syntax/marble-syntax.spec.ts
@@ -2,12 +2,16 @@ import { cold } from 'jasmine-marbles';
 import { NEVER, EMPTY } from 'rxjs';
 
 describe('Marble Syntax', () => {
-  it('emits no items but terminates normally', () => {
-    expect(EMPTY).toBeObservable(cold('|'));
+  describe('EMPTY', () => {
+    it('emits no items but terminates normally', () => {
+      expect(EMPTY).toBeObservable(cold('|'));
+    });
   });
 
-  it('emits no items and does not terminate', () => {
-    expect(NEVER).toBeObservable(cold('-'));
-    expect(NEVER).toBeObservable(cold('-----'));
+  describe('NEVER', () => {
+    it('emits no items and does not terminate', () => {
+      expect(NEVER).toBeObservable(cold('-'));
+      expect(NEVER).toBeObservable(cold('-----'));
+    });
   });
 });
